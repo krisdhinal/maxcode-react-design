b7801d43a61649739b2c0ebaaf95572ac4aa54bb
Kris Dhinal <krisdhinal@gmail.com>
2025-01-07 20:08:24 +0700

    implemen barcode main menu and implemen sub menu in barcode main menu, plus adding new icon for main menu and sub menu

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ const Dashboard = ({ onLogout }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [barcodeSubmenuOpen, setBarcodeSubmenuOpen] = useState(true); // Added state for barcode submenu
 
   const handleLogout = () => {
     logout();
@@ -77,9 +78,12 @@ const Dashboard = ({ onLogout }) => {
         </div>
         <div className="p-4">
           <ul>
-            {/* Home */}
+            {/* Barcode Menu */}
             <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-              <div className="flex items-center space-x-2">
+              <button 
+                onClick={() => setBarcodeSubmenuOpen(!barcodeSubmenuOpen)}
+                className="w-full flex items-center space-x-2"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className={`w-6 h-6 text-white ${!sidebarOpen && "mx-auto"}`}
@@ -91,51 +95,99 @@ const Dashboard = ({ onLogout }) => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M3 9L12 2l9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                    d="M4 6h16M4 10h16M4 14h16M4 18h16"
                   />
                 </svg>
-                {sidebarOpen && <span>Home</span>}
-              </div>
-            </li>
-            {/* Settings */}
-            <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-              <div className="flex items-center space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={`w-6 h-6 text-white ${!sidebarOpen && "mx-auto"}`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm-4-5a1 1 0 011-1h6a1 1 0 011 1v1a1 1 0 01-1 1h-6a1 1 0 01-1-1V3zm4 18c1.7 0 3.18-.68 4.25-1.75A6.978 6.978 0 0017 14H7a6.978 6.978 0 00-3.25 5.25C8.82 19.32 10.99 20 12 20z"
-                  />
-                </svg>
-                {sidebarOpen && <span>Settings</span>}
-              </div>
-            </li>
-            {/* Reports */}
-            <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-              <div className="flex items-center space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={`w-6 h-6 text-white ${!sidebarOpen && "mx-auto"}`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 7v14a1 1 0 001 1h16a1 1 0 001-1V7a1 1 0 00-1-1H4a1 1 0 00-1 1zm0 4h16M3 5h16"
-                  />
-                </svg>
-                {sidebarOpen && <span>Reports</span>}
-              </div>
+                {sidebarOpen && (
+                  <>
+                    <span>Barcode</span>
+                    <svg
+                      className={`w-4 h-4 ml-auto transition-transform duration-200 ${
+                        barcodeSubmenuOpen ? "transform rotate-180" : ""
+                      }`}
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </>
+                )}
+              </button>
+              {sidebarOpen && barcodeSubmenuOpen && (
+                <ul className="pl-8 mt-2 space-y-2">
+                  {/* Receive Submenu */}
+                  <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                    <div className="flex items-center space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-5 h-5 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8"
+                        />
+                      </svg>
+                      <span>Receive</span>
+                    </div>
+                  </li>
+                  {/* Reports Submenu */}
+                  <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                    <div className="flex items-center space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-5 h-5 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+                        />
+                      </svg>
+                      <span>Print</span>
+                    </div>
+                  </li>
+                  {/* Settings Submenu */}
+                  <li className="text-sm text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                    <div className="flex items-center space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-5 h-5 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                      <span>Print Setting</span>
+                    </div>
+                  </li>
+                </ul>
+              )}
             </li>
           </ul>
         </div>
